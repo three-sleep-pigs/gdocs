b18590092d3663615c6bfd05f3c53af80bb6f4f5
xjqnlxx <xjqnlxx@outlook.com>
2021-07-06 20:20:02 +0800

    add: gdocs frontend frame

---
+++ code/gdocs/src/View/MenuView.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+class MenuView extends React.Component{
+    render() {
+        return(
+            <div className="menu-page">
+
+            </div>
+        );
+    }
+}
+
+export default MenuView;
