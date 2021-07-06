93c1621416a26bde3bc49e1f5d48644d4da825df
riken01 <riken01@sjtu.edu.cn>
2021-07-06 17:16:33 +0800

    add: gDocs frontend

---
+++ gdocs/src/View/ExcelView.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+class ExcelView extends React.Component{
+    render() {
+        return(
+            <div className="excel-page">
+
+            </div>
+        );
+    }
+}
+
+export default ExcelView;
