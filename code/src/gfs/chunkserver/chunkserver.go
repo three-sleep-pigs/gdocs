9d4ea2413b021550b8e6a2242adceea55de8d4d5
xjqnlxx <xjqnlxx@outlook.com>
2021-07-11 19:56:22 +0800

    fix: master and chunk server bugs

--- code/src/gfs/chunkserver/chunkserver.go
+++ code/src/gfs/chunkserver/chunkserver.go
@@ -6,7 +6,6 @@ import (
 	"encoding/gob"
 	"fmt"
 	"io"
-	"log"
 	"net"
 	"net/rpc"
 	"os"
@@ -427,6 +426,33 @@ func (cs *ChunkServer) RPCSendCopy(args gfs.SendCopyArg, reply *gfs.SendCopyRepl
 	return nil
 }
 
+// RPCApplyCopy called by main chunk server
+// copy the data from the beginning to chunk handle
+func (cs *ChunkServer) RPCApplyCopy(args gfs.ApplyCopyArg, reply *gfs.ApplyCopyReply) error {
+	handle := args.Handle
+	//get the chunk to handle
+	ckF, ok := cs.chunks.Get(fmt.Sprintf("%d", handle))
+	if !ok {
+		return fmt.Errorf("chunk %v does not exist", handle)
+	}
+	ck := ckF.(*ChunkInfo)
+	ck.lock.Lock()
+	defer ck.lock.Unlock()
+	if ck.invalid {
+		return fmt.Errorf("chunk %v is invalid", handle)
+	}
+	//set the version
+	ck.version = args.Version
+	// FIXME: should file be created?
+	// write chunk func doesn't create files
+	//write the data at a new chunk's beginning
+	err := cs.writeChunk(handle, args.Data, 0)
+	if err != nil {
+		return err
+	}
+	return nil
+}
+
 // called by RPCReadChunk, chunk.lock is locked in top caller
 //read data from a chunk according to offset and length of the data slice
 func (cs *ChunkServer) readChunk(handle int64, offset int64, data []byte, length *int) error {
@@ -448,25 +474,23 @@ func (cs *ChunkServer) writeChunk(handle int64, data []byte, offset int64) error
 	if len > gfs.MaxChunkSize {
 		// FIXME: we have no log now
 		//log.Fatal("Maximum chunk size exceeded!")
-		return fmt.Errorf("[chunk server] maxinum chunk size exceeded")
+		return fmt.Errorf("maxinum chunk size exceeded")
 	}
 
 	//open the chunk file
 	filename := path.Join(cs.rootDir, fmt.Sprintf("chunk%v.chk", handle))
-	// FIXME: os.O_CREATE is wrong, writeChunk shouldn't create file
 	// Consider writeChunk and deleteChunk happen concurrently, writeChunk shouldn't create file after os.Remove
-	file, err := os.OpenFile(filename, os.O_WRONLY|os.O_CREATE, 0755)
+	file, err := os.OpenFile(filename, os.O_WRONLY, 0644)
 	if err != nil {
-		return err
+		return fmt.Errorf("open file err %s", err)
 	}
 	defer file.Close()
 
 	//write the chunk file at the offset
 	_, err = file.WriteAt(data, offset)
 	if err != nil {
-		return err
+		return fmt.Errorf("write file err %s", err)
 	}
-
 	return nil
 }
 
@@ -487,13 +511,13 @@ func (cs *ChunkServer) sync(handle int64, ck *ChunkInfo, m *Mutation) error {
 		ck.length = len
 	}
 	//TODO: update the checksum
+
 	return nil
 
 }
 
-
-// rpc called by client
-// write the data at given offet of args to chunkhandle
+// RPCWriteChunk called by client
+// write the data at given offset of args to chunk handle
 func (cs *ChunkServer) RPCWriteChunk(args gfs.WriteChunkArg, reply *gfs.WriteChunkReply) error {
 	//get the data block from data buffer and delete the block
 	data, err := cs.db.Fetch(args.DbID)
@@ -504,48 +528,47 @@ func (cs *ChunkServer) RPCWriteChunk(args gfs.WriteChunkArg, reply *gfs.WriteChu
 	//determine if the size after writing is larger than MaxChunkSize,if so,return errors
 	newLength := args.Offset + int64(len(data))
 	if newLength > gfs.MaxChunkSize {
-		return fmt.Errorf("Maximum chunk size exceeded!")
+		return fmt.Errorf("maximum chunk size exceeded")
 	}
 
 	//get the chunk
 	handle := args.DbID.Handle
-	cs.lock.RLock()
-	ck, ok := cs.chunk[handle]
-	if !ok || ck.invalid {
-		cs.lock.RUnlock()
-		return fmt.Errorf("Chunk %v does not exist or is abandoned", handle)
+	ckF, ok := cs.chunks.Get(fmt.Sprintf("%d", handle))
+	if !ok {
+		return fmt.Errorf("chunk %v does not exist", handle)
 	}
-	cs.lock.RUnlock()
-	//lock the chunk
+	ck := ckF.(*ChunkInfo)
 	ck.lock.Lock()
 	defer ck.lock.Unlock()
+	if ck.invalid {
+		return fmt.Errorf("chunk %v is invalid", handle)
+	}
 
 	//add mutation
 	mutation := &Mutation{data, args.Offset}
 
-	//concurrent call sync at main chunkserver
+	//concurrent call sync at main chunk server
 	wait := make(chan error, 1)
 	go func() {
 		wait <- cs.sync(handle, ck, mutation)
 	}()
 
-	err = <-wait
+	//send apply Mutation call to all secondaries
+	applyMutationArgs := gfs.ApplyMutationArg{DbID: args.DbID, Offset: args.Offset}
+	err = gfs.CallAll(args.Secondaries, "ChunkServer.RPCApplyMutation", applyMutationArgs)
 	if err != nil {
 		return err
 	}
 
-	//send apply Mutation call to all sencondaries
-	applyMutationArgs := gfs.ApplyMutationArg{args.DbID, args.Offset}
-	err = gfs.CallAll(args.Secondaries, "ChunkServer.RPCApplyMutation", applyMutationArgs)
-
+	err = <-wait
 	if err != nil {
 		return err
 	}
 	return nil
 }
 
-// rpc called by client
-// write the data of args at the end of chunkhandle
+// RPCAppendChunk called by client
+// write the data of args at the end of chunk handle
 func (cs *ChunkServer) RPCAppendChunk(args gfs.AppendChunkArg, reply *gfs.AppendChunkReply) error {
 	//get the data block from data buffer and delete the block
 	data, err := cs.db.Fetch(args.DbID)
@@ -555,22 +578,21 @@ func (cs *ChunkServer) RPCAppendChunk(args gfs.AppendChunkArg, reply *gfs.Append
 
 	//determine if the size of append data is larger than MaxChunkSize/4,if so,return errors
 	if len(data) > gfs.MaxAppendSize {
-		return fmt.Errorf("Maximum chunk append size excceeded!")
+		return fmt.Errorf("maximum chunk append size excceeded")
 	}
 
 	//get the chunk
 	handle := args.DbID.Handle
-	cs.lock.RLock()
-	ck, ok := cs.chunk[handle]
-
-	if !ok || ck.invalid {
-		cs.lock.RUnlock()
-		return fmt.Errorf("Chunk %v does not exist or is abandoned", handle)
+	ckF, ok := cs.chunks.Get(fmt.Sprintf("%d", handle))
+	if !ok {
+		return fmt.Errorf("chunk %v does not exist", handle)
 	}
-	cs.lock.RUnlock()
-	//lock the chunk
+	ck := ckF.(*ChunkInfo)
 	ck.lock.Lock()
 	defer ck.lock.Unlock()
+	if ck.invalid {
+		return fmt.Errorf("chunk %v is invalid", handle)
+	}
 
 	newLen := ck.length + int64(len(data)) //size after writing
 	offset := ck.length                    //write at the last of data
@@ -595,50 +617,22 @@ func (cs *ChunkServer) RPCAppendChunk(args gfs.AppendChunkArg, reply *gfs.Append
 		wait <- cs.sync(handle, ck, mutation)
 	}()
 
-	err = <-wait
-	if err != nil {
-		return err
-	}
-
 	// call secondaries
-	applyMutationArgs := gfs.ApplyMutationArg{args.DbID, offset}
+	applyMutationArgs := gfs.ApplyMutationArg{DbID: args.DbID, Offset: offset}
 	err = gfs.CallAll(args.Secondaries, "ChunkServer.RPCApplyMutation", applyMutationArgs)
 	if err != nil {
 		return err
 	}
 
-	return nil
-}
-
-// rpc called by main chunk server
-// copy the data from the beginning to chunkhandle
-func (cs *ChunkServer) RPCApplyCopy(args gfs.ApplyCopyArg, reply *gfs.ApplyCopyReply) error {
-	handle := args.Handle
-	//get the chunk to handle
-	cs.lock.RLock()
-	ck, ok := cs.chunk[handle]
-
-	if !ok || ck.invalid {
-		cs.lock.RUnlock()
-		return fmt.Errorf("Chunk %v does not exist or is abandoned", handle)
-	}
-	cs.lock.RUnlock()
-	//lock the chunk
-	ck.lock.Lock()
-	defer ck.lock.Unlock()
-
-	//set the version
-	ck.version = args.Version
-
-	//write the data at a new chunk's beginning
-	err := cs.writeChunk(handle, args.Data, 0)
+	err = <-wait
 	if err != nil {
 		return err
 	}
+
 	return nil
 }
 
-// rpc called by main chunk server
+// RPCApplyMutation called by main chunk server
 // write the data at the given offset to chunkhandle
 func (cs *ChunkServer) RPCApplyMutation(args gfs.ApplyMutationArg, reply *gfs.ApplyMutationReply) error {
 	//get data
@@ -649,19 +643,17 @@ func (cs *ChunkServer) RPCApplyMutation(args gfs.ApplyMutationArg, reply *gfs.Ap
 
 	//get the chunk
 	handle := args.DbID.Handle
-	cs.lock.RLock()
-	ck, ok := cs.chunk[handle]
-	
-	if !ok || ck.invalid {
-		cs.lock.RUnlock()
-		return fmt.Errorf("cannot find chunk %v", handle)
+	ckF, ok := cs.chunks.Get(fmt.Sprintf("%d", handle))
+	if !ok {
+		return fmt.Errorf("chunk %v does not exist", handle)
 	}
-	cs.lock.RUnlock()
-	mutation := &Mutation{data, args.Offset}
-
-	//lock the chunk
+	ck := ckF.(*ChunkInfo)
 	ck.lock.Lock()
 	defer ck.lock.Unlock()
+	if ck.invalid {
+		return fmt.Errorf("chunk %v is invalid", handle)
+	}
+	mutation := &Mutation{data, args.Offset}
 
 	//apply mutation
 	err = cs.sync(handle, ck, mutation)
