58eecd543c2c199b4e7de2e60f10635decca77c1
Mengqi Guo <46805856+gmq0508@users.noreply.github.com>
2021-07-15 12:39:53 +0800

    update frontend

--- code/gdocs/src/View/ExcelView.js
+++ code/gdocs/src/View/ExcelView.js
@@ -1,15 +1,19 @@
 import React from "react";
 import Luckysheet from '../Component/Luckysheet'
+import {withRouter} from "react-router-dom";
 class ExcelView extends React.Component{
+    constructor(props) {
+        super(props);
+    }
     render() {
         return(
             <div className="excel-page">
                 <header className="App-header">
-                    <Luckysheet/>
+                    <Luckysheet id={this.props.location.state.id} filename={this.props.location.state.filename}/>
                 </header>
             </div>
         );
     }
 }
 
-export default ExcelView;
+export default withRouter(ExcelView);
